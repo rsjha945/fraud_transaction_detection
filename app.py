fa94341238e7bea61ec2962ba1f3f218cc7c4f82
rsjha945 <ravishankarjha7108gmail.com>
2022-09-01 21:25:17 +0530

    logger and exception has been completed

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+import sys
+from fraud.logger import logging
+from fraud.exception import FraudException
+
 
 app=Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        fraud = FraudException(e,sys)
+        logging.info(fraud.error_message)
+        logging.info("We are testing logging module")
     return "CI CD pipeline has been established."
 
 
